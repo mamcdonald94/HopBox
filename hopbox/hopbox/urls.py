66341c71a34494c80d06cb8e5a005e19e8e13fdb
Rick Canham <rickcanham@hotmail.com>
2021-08-20 22:15:57 -0700

    Added my functions to views.py and updated a few variable names in HTML files

--- hopbox/hopbox/urls.py
+++ hopbox/hopbox/urls.py
@@ -1,4 +1,4 @@
-"""hopbox_app URL Configuration
+"""hopbox URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('home',views.disp_home),
-    path('cart',views.disp_cart), 
-    path('option/<int:optionNum>',views.disp_option),
+    path('admin/', admin.site.urls),
+    path('',include('hopbox_app.urls')),
 ]
